daba24b4f16c86b8dcc5592453909e9d333a11d8
Rashmika Siriwardhana <58144396+rnsrashmika078@users.noreply.github.com>
2026-01-29 12:48:35 +0530

    remove playright

--- app/api/ai/agent/route.ts
+++ app/api/ai/agent/route.ts
@@ -5,33 +5,11 @@ export async function POST(req: NextRequest) {
   try {
     const prompt = await req.json();
 
-    console.log("prompt", prompt);
-    // if (!prompt) return NextResponse.json({ message: "prompt not found" });
-    // const client = new OpenAI({
-    //   baseURL: "https://router.huggingface.co/v1",
-    //   apiKey: process.env.HF_TOKEN,
-    // });
-
-    // const chatCompletion = await client.chat.completions.create({
-    //   model: "zai-org/GLM-4.7:novita",
-    //   messages: [
-    //     {
-    //       role: "user",
-    //       content: prompt,
-    //     },
-    //   ],
-    // });
-
-    // // const text = output.content;
-    // const text = chatCompletion.choices[0].message.content;
-
     const key = process.env.BYTEZ!;
     const sdk = new Bytez(key);
 
-    // choose gpt-oss-20b
     const model = sdk.model("deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B")
 
-    // send input to model
     const { error, output } = await model.run([
       {
         role: "user",
